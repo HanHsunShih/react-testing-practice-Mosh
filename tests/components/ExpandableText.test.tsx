882043cbdf68057fd310c499b75e3c6ea0c5fe02
Han Hsun Shih (Amy) <a23514788@gmail.com>
2025-05-20 19:03:26 +0100

    Add collapse text test in ExpandableText component

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import ExpandableText from "../../src/components/ExpandableText";
+import userEvent from "@testing-library/user-event";
+
 describe("group", () => {
+  const limit = 255;
+  const longText = "a".repeat(limit + 1);
+  const truncateText = longText.substring(0, limit) + "...";
+
   it("shouldnt render full text if text is less than limit", () => {
     const text = "Short Text";
 
@@ -10,16 +16,37 @@ describe("group", () => {
   });
 
   it("should truncate if text is longer than limit", () => {
-    const text = "a".repeat(256);
+    render(<ExpandableText text={longText} />);
 
-    render(<ExpandableText text={text} />);
-
-    const truncatedText = text.substring(0, 255) + "...";
+    const truncatedText = truncateText;
     const button = screen.getByRole("button");
 
     expect(screen.getByText(truncatedText)).toBeInTheDocument();
     expect(button).toHaveTextContent(/show more/i);
   });
 
-  it("should reveal full text if ", () => {});
+  it("should expand text when user click the show more button", async () => {
+    render(<ExpandableText text={longText} />);
+
+    const button = screen.getByRole("button");
+    const user = userEvent.setup();
+
+    await user.click(button);
+
+    expect(screen.getByText(longText)).toBeInTheDocument();
+    expect(button).toHaveTextContent(/less/i);
+  });
+
+  it("should collapse the text when user click show less button", async () => {
+    render(<ExpandableText text={longText} />);
+
+    const button = screen.getByRole("button");
+    const user = userEvent.setup();
+
+    await user.click(button);
+    await user.click(button);
+
+    expect(screen.getByText(truncateText)).toBeInTheDocument();
+    expect(button).toHaveTextContent(/more/i);
+  });
 });
